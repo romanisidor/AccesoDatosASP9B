2621ea0deab8edfc4c332a0530417b0da48a0f46
Román Isidor <18307024@utcgg.edu.mx>
2021-06-28 07:30:52 -0500

    Primer commit, 1 solo WebForm1 ya hay consultas a SQL Server

---
+++ WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebAplicacionConexiconSQL.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script src="js/micodigo.js"></script>
+    <script src="js/sweetalert2.all.min.js"></script>
+    <link href="css/sweetalert2.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Probar conexión" Width="791px" OnClick="Button1_Click" />
+            <br />
+            <br />
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server" Width="785px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Consulta con DataReader" Width="791px" />
+        <br />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" Height="122px" Width="792px"></asp:ListBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Consulta con DataSet" Width="790px" />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Obtener datos del DataSet" Width="791px" />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" Width="791px">
+        </asp:GridView>
+        <br />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using ConexionBD;
+namespace WebAplicacionConexiconSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        ClassAccesoSQL objacc = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                objacc = new ClassAccesoSQL("Data Source = ROMANISIDOR; Initial Catalog = BDTIENDA; Integrated Security = true");
+                Session["objacc"] = objacc;
+            }
+            else
+            {
+                objacc = (ClassAccesoSQL)Session["objacc"];
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection temp = null;
+            string mensaje = "";
+            temp = objacc.AbrirConexion(ref mensaje);
+            //TextBox1.Text = mensaje;
+            if (temp != null)
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(), 
+                    "msgbox01", "verAlerta('Conexión correcta ', '"+ mensaje +"', 'success');", true);
+                temp.Close(); //Cerrar la conexión
+                temp.Dispose(); //Destruir el objeto de conexión
+
+            }
+            else
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                    "msgbox01", "verAlerta(`Conexión incorrecta `, `" + mensaje + "`, `error`);", true);
+            }
+           
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlDataReader caja = null;
+
+            SqlConnection cnab = null;
+
+            string m = "";
+
+            cnab = objacc.AbrirConexion(ref m);
+
+            if (cnab != null)
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                    "123", "verAlerta('Correcto', '" + m + "', 'success')", true);
+
+                caja = objacc.ConsultarReader("select * from EMPLEADO", cnab, ref m);
+
+                if (caja != null)
+                //La consuta es correcta y se leen los datos
+                {
+                    ListBox1.Items.Clear();
+                    while (caja.Read())
+                    {
+                        ListBox1.Items.Add(caja[0] + " " + caja["NOMBRE"]);
+
+                    }
+
+                }
+
+            }
+            else
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                   "1234", "verAlerta('Incorrecto', '" + m + "', 'error')", true);
+
+            }
+
+            cnab.Close();
+            cnab.Dispose();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            DataSet contenedor = null;
+
+            SqlConnection cnab = null;
+
+            string m = "";
+
+            cnab = objacc.AbrirConexion(ref m);
+
+            if (cnab != null)
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                    "msg123", "verAlerta('Correcto', '" + m + "', 'success')", true);
+
+                contenedor = objacc.ConsultaDS("select * from EMPLEADO", cnab, ref m);
+
+                cnab.Close();
+                cnab.Dispose();
+                if (contenedor != null)
+                //La consuta es correcta y se leen los datos
+                {
+
+                    GridView1.DataSource = contenedor.Tables[0];
+                    GridView1.DataBind();
+                    Session["Tabla1"] = contenedor;
+
+                }
+
+            }
+            else
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                   "msg1234", "verAlerta('Incorrecto', '" + m + "', 'error')", true);
+
+            }
+
+
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            DataSet temp = Session["Tabla1"] as DataSet;
+            ListBox1.Items.Clear();
+
+            ListBox1.Items.Add("Datos recuperados del dataTable 0");
+
+
+
+            foreach (DataRow registro in temp.Tables[0].Rows)
+            {
+                ListBox1.Items.Add(registro[0] + " -- " + registro[1]);
+            }
+        }
+    }
+}
